e15942c495cdfb84442c06971899e6a65e1c243c
globalstudios <globalstudios.e@gmail.com>
2015-09-28 02:28:23 +0900

    Add responsive style for tablet

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@ $(function(){
 
 	$("a[href^=#]").click(function() {
 	  var speed = 500;
-	  var nav_height = 48;
+	  var nav_height = $(".local-nav").height();
 	  var href= $(this).attr("href");
 	  var target = $(href == "#" || href == "" ? "html" : href);
 	  var position = target.offset().top - nav_height;
